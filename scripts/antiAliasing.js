7d67ae2ccd85b7a5ec38b32058e35f10e8fe86d2
Devam-Pathak-108 <devam2424@gmail.com>
2024-10-05 22:42:49 +0530

    Finally something that works

---
+++ scripts/antiAliasing.js
@@ -0,0 +1,5 @@
+function applyAntiAliasing(p1, p2, ctx, gridSize) {
+  // Implement Xiaolin Wu's anti-aliasing algorithm logic here
+  // This will require fractional pixel intensities
+  ctx.fillStyle = "rgba(255, 0, 0, 0.5)"; // Example anti-aliasing effect
+}
